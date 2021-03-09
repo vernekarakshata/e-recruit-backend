0f9814a3a5b97e9a8d0e12cf083e952f94e236e7
akshata <akshatav10@gmail.com>
2021-03-09 19:19:11 +0530

    initial commit

---
+++ src/main/java/com/hack/shesquad/ERecruitApplication.java
@@ -0,0 +1,13 @@
+package com.hack.shesquad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERecruitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ERecruitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hack/shesquad/ERecruitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hack.shesquad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERecruitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
